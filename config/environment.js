34fec0c973d08417dd0cbc74dcf37873dd52fe46
lawton b <lawton.a.browning@gmail.com>
2016-04-07 11:09:27 -0700

    add content securit policy and firebase adress

--- config/environment.js
+++ config/environment.js
@@ -4,6 +4,7 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'wm-site',
     environment: environment,
+    firebase: 'https://wmsite.firebaseio.com/',
     baseURL: '/',
     locationType: 'auto',
     EmberENV: {
@@ -16,6 +17,15 @@ module.exports = function(environment) {
     APP: {
       // Here you can pass flags/options to your application instance
       // when it is created
+    },
+    contentSecurityPolicy: {
+      'default-src': "'none'",
+      'script-src': "'self' 'unsafe-eval' https://*.firebaseio.com",
+      'font-src': "'self'",
+      'connect-src': "'self' wss://*.firebaseio.com",
+      'img-src': "'self' *",
+      'style-src': "'self' 'unsafe-inline'",
+      'frame-src': "'self' https://*.firebaseio.com"
     }
   };
 
