2f1d0350efef32ae3561633245031b83a10bdcf1
lawton browning <lawton.a.browning@gmail.com>
2016-04-12 15:14:26 -0700

    display album image in structure selected album view

--- app/router.js
+++ app/router.js
@@ -9,6 +9,7 @@ Router.map(function() {
   this.route('admin');
   this.route('about');
   this.route('albums');
+  this.route('structure-selected-album', {path: '/structure-selected-album/:album_id'});
 });
 
 export default Router;
--- app/routes/albums.js
+++ app/routes/albums.js
@@ -1,7 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model(){
-    return this.store.findAll('album');
+  model() {
+      return this.store.findAll('album');
     }
-  })
+  });
+
+
+
+
+//   model(){
+//     return this.store.findAll('album');
+//   }
+// })
