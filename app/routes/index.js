0a57677b550531f5bcdceea56c050f7f863b08da
Jeff Seymour and lawton browning <lawton@>
2016-04-13 09:19:44 -0700

    add show view on index

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-});
+  model(){
+    return Ember.RSVP.hash({
+      albums: this.store.findAll('album'),
+      artworks: this.store.findAll('artwork'),
+      performances: this.store.findAll('performance'),
+      pressclips: this.store.findAll('pressclip'),
+      products: this.store.findAll('product'),
+      songs: this.store.findAll('song'),
+      videos: this.store.findAll('video'),
+    });
+  }
+})
