58fdd5711bb92115736a5f55996d21a9d4d280c1
dmdiehr <ddiehr@yahoo.com>
2016-04-13 22:34:17 -0700

    Updates new-performance component to input datetime, and input multiple confederates.

--- app/components/admin-new-product.js
+++ app/components/admin-new-product.js
@@ -16,7 +16,7 @@ export default Ember.Component.extend({
       this.set('cents', "");
     },
     saveNewProduct(){
-      var correctPrice = parseInt(this.get('dollars')) * 100 + parseInt(this.get('cents'))
+      var correctPrice = parseInt(this.get('dollars')) * 100 + parseInt(this.get('cents'));
       var params = {
 
         number: this.get('number'),
